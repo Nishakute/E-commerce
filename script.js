57596ee458abc1ca3133b7847de96ec8ada573cf
Nishakute <kuteenisha0405@gmail.com>
2024-06-03 23:44:15 +0530

    "CHANGES"

--- script.js
+++ script.js
@@ -59,16 +59,41 @@ products = [
                                 <h5 class="card-title">${element.productName}</h5>
                                 <p class="card-text">${element.discription}</p>
                                 <h4 class="card-text">&#x20b9; ${element.price}</h4>
-                                <button class="btn btn-primary" onclick="addToCart('${element.id}')">Add to cart</button>
+                                <button class="btn btn-primary" onclick="addToCart(${element.id})">Add to cart</button>
                             </div>
                         </div>
         `;
         showCarts.appendChild(rowDiv);
     });
 
-    Cart=[];
+    cart=[];
+    
 
     function addToCart(ID){
         console.log(ID);
-        indexNumber=products.findIndex(element=> element.ID)
+        indexNumber= products.findIndex(element=> element.id === ID)
+        if(indexNumber != -1){
+          cart.push(products[indexNumber]);
+          console.log(cart);
+          console.log(cart.length);
+        }
+         else{
+           alert("product out of stock");
+        }
+        document.getElemetById('menuCountInCart').innerHTML= cart.length;
     }
+
+    document.getElementById('showCartOnModal').addEventListener('click',()=>{
+
+    cart.forEach((element)=>{
+      cartList=document.createElement('div')
+      cartList.innerHTML=`<div>
+      <span style="font-size:bold">${element.productName}</span>
+      <span>&#x20b9,${element.price}</span>
+    </div>`;
+      document.getElementById('prdouctsInCart').appendChild(cartList);
+    })
+  });
+
+
+
